1bdc08ab4d5a864b2a529f05a2e3d1fe2e3dbe1e
Farizha Aulia Martakusuma <farizha.aulia@gmail.com>
2022-01-08 00:57:41 +0100

    adding FavoriteMovies

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -5,7 +5,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { GenreCardComponent } from '../genre-card/genre-card.component';
 import { DirectorCardComponent } from '../director-card/director-card.component';
-import { MovieViewComponent } from '../movie-view/movie-view.component';
+import { SynopsisCardComponent } from '../synopsis-card/synopsis-card.component';
 
 @Component({
   selector: 'app-movie-card',
@@ -16,6 +16,8 @@ export class MovieCardComponent {
   
   user: any = JSON.parse(localStorage.getItem('user') || '');
   movies: any[] = [];
+  favMovies: any[] = this.user.FavoriteMovies;
+
   constructor(
     public fetchApiData: FetchApiDataService,
     public dialog: MatDialog,
@@ -25,6 +27,7 @@ export class MovieCardComponent {
 
   ngOnInit(): void {
     this.getMovies();
+    this.getUserFavs();
   }
 
   getMovies(): void {
@@ -49,10 +52,59 @@ export class MovieCardComponent {
     });
   }
 
-  openMovieViewDialog(name: string, description: string, year: any,): void {
-    this.dialog.open(MovieViewComponent, {
+  openSynopsisCardDialog(name: string, description: string, year: any,): void {
+    this.dialog.open(SynopsisCardComponent, {
       data: {name, description, year},
       width: '500px',
     });
+  }
+
+  getUserFavs(): any {
+    this.fetchApiData.getFavMovies(this.user.Username).subscribe((res: any) => {
+      this.favMovies = res.Favorites;
+      return this.favMovies;
+    });
+  }
+
+  addToFavs(movieId: string, title: string): void {
+    this.fetchApiData
+      .addToFav(this.user.Username, movieId)
+      .subscribe((res: any) => {
+        this.snackBar.open(
+          `${title} has been added to your favorite movies!`,
+          'Okay',
+          {
+            duration: 2000,
+          }
+        );
+        this.ngOnInit();
+      });
+    return this.getUserFavs();
+  }
+
+  removeFromFavs(movieId: string, title: string): void {
+    this.fetchApiData
+      .removeFromFav(this.user.Username, movieId)
+      .subscribe((res: any) => {
+        this.snackBar.open(
+          `${title} has been removed from your favorite movies`,
+          'Okay',
+          {
+            duration: 2000,
+          }
+        );
+        this.ngOnInit();
+      });
+    return this.getUserFavs();
+  }
+
+  isFav(movieId: string): boolean {
+    return this.favMovies.some((movie) => movie._id === movieId);
+  }
+
+  toggleFavs(movie: any): void {
+    this.isFav(movie._id)
+      ? this.removeFromFavs(movie._id, movie.Title)
+      : this.addToFavs(movie._id, movie.Title);
   }
 }
